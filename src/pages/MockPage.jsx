8411bc5f682d469f4108c66dee38848497406c6d
Cori1304-Seong <andyjm7739@gmail.com>
2025-06-10 00:09:23 +0900

    feat: MockPage 추가
    이동경로 지정할 곳이 아직 없다면 보여질 페이지입니다.

---
+++ src/pages/MockPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MockPage() {
+    return (
+        <div className="page">
+            <h2>MockPage</h2>
+            <p>이동 경로 지정을 못 했다면 이 페이지가 보입니다.</p>
+        </div>
+    );
+}
+
+export default MockPage;
