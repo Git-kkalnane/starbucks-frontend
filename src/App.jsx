833ffee1ec7d6c6127d4c03629a7cb65d7102c85
Cori1304-Seong <andyjm7739@gmail.com>
2025-06-09 09:12:21 +0900

    feat: Add app

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './navigation/Navigation';
+import Home from './pages/Home';
+import Pay from './pages/Pay';
+import Order from './pages/Order';
+import Other from './pages/Other';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+import './App.css';
+
+function App() {
+    return (
+        <div className="app">
+            <Navigation />
+            <main className="page-container">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/pay" element={<Pay />} />
+                    <Route path="/order" element={<Order />} />
+                    <Route path="/other" element={<Other />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                </Routes>
+            </main>
+        </div>
+    );
+}
+
+export default App;
