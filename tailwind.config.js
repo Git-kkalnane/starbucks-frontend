62a377790382435ddb7a053f8cafaddd4635d557
Cori1304-Seong <andyjm7739@gmail.com>
2025-06-12 01:12:14 +0900

    style: tailwind에 색상 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Pretendard", "sans-serif"],
-      },
-      colors: {
-        "bg-app": "#f6f5ef", // 배경색
-        "starbucks-green": "#007240",
-      },
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Pretendard', 'sans-serif'],
+            },
+            colors: {
+                'bg-app': '#f6f5ef', // 배경색
+                'starbucks-green': '#01A862',
+                'able-string': '#48b77a',
+                'disabled-string': '#8b8b8b',
+                deactivation: '#e0e0e0',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
