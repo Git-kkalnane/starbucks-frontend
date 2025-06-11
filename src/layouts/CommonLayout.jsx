0c50a507d207a1676df41cd0308672246283ac26
Cori1304-Seong <andyjm7739@gmail.com>
2025-06-12 01:09:55 +0900

    style:  CommonLayout 안에 Pretendard 폰트 적용

--- src/layouts/CommonLayout.jsx
+++ src/layouts/CommonLayout.jsx
@@ -3,5 +3,5 @@ import React from 'react';
 import './Layout.css'; // 스타일 분리
 
 export default function CommonLayout({ children, className = '' }) {
-    return <div className={`layout-container ${className}`}>{children}</div>;
+    return <div className={`layout-container font-[Pretendard] ${className}`}>{children}</div>;
 }
