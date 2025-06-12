ec9ff5205051b4d85c0d9e22ae02aa8ead7f8a27
Cori1304-Seong <andyjm7739@gmail.com>
2025-06-13 02:20:33 +0900

    feat: MenuList 추가

---
+++ src/components/order/MenuList.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const MenuItem = ({ item, className }) => {
+    return (
+        <div className={`flex py-2 border-b border-gray-100 ${className}`}>
+            <div className="w-24 h-24 rounded-full bg-gray-100 mr-4 flex-shrink-0"></div>
+            <div className="flex-1">
+                <div className="font-medium mt-4">{item.koreanName}</div>
+                <div className="text-gray-600 text-sm mb-1">{item.englishName}</div>
+                <div className="font-medium text-gray-800">{item.price.toLocaleString()}원</div>
+            </div>
+        </div>
+    );
+};
+
+const MenuList = ({ activeTab, drinkItems, foodItems, className }) => {
+    return (
+        <section className={`px-1 ${className}`}>
+            <div className="space-y-4 overflow-y-auto h-[calc(100vh-220px)]">
+                {activeTab === '음료' && drinkItems.map((item) => <MenuItem key={item.id} item={item} />)}
+                {activeTab === '푸드' && foodItems.map((item) => <MenuItem key={item.id} item={item} />)}
+            </div>
+        </section>
+    );
+};
+
+export default MenuList;
