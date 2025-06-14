6b2be85d1784f14811c32cde82264cb82d6e0454
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-06-14 16:27:10 +0900

    fix: 디자인 수정
    - UnitItem의 불필요한 tailwind 클래스 제거
    - customBtn의 border 컬러를 수정

--- src/components/charge/UnitItem.jsx
+++ src/components/charge/UnitItem.jsx
@@ -2,7 +2,7 @@ export const UnitItem = ({ unit, selectedUnit, onChangeUnit, onClick }) => {
     return (
         <label
             className={`flex justify-center py-2 text-lg border-[#C0C0C0] border rounded-lg unit-btn ${
-                selectedUnit === unit.value ? 'bg-[#01A862] border-[#01A862]' : 'hover:bg-blue-100 border-[#C0C0C0]'
+                selectedUnit === unit.value ? 'bg-[#01A862] border-[#01A862]' : 'border-[#C0C0C0]'
             }`}
             key={unit.name}
         >
