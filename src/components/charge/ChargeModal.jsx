890f3024a426e49b4e2a4b5394202e4205daf9f5
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-06-15 12:34:16 +0900

    refactor: ChargeModal 컴포넌트 분리
    ChargeModal 컴포넌트의 코드 길이가 다소 길다고 판단되어 내부에 있던 요소들을 별도의 컴포넌트로 분리하였다.

--- src/components/charge/ChargeModal.jsx
+++ src/components/charge/ChargeModal.jsx
@@ -7,8 +7,6 @@ export const ChargeModal = ({ isModalOpen, setIsModalOpen, setSelectedUnit, modi
         setValue(e.target.value);
     };
 
-    const isButtonEnabled = value.trim() !== '';
-
     return (
         <div>
             {isModalOpen && (
@@ -18,53 +16,73 @@ export const ChargeModal = ({ isModalOpen, setIsModalOpen, setSelectedUnit, modi
                         onClick={(e) => e.stopPropagation()}
                     >
                         <h2 className="px-6 mb-4 text-lg font-semibold">1만원부터 55만원 까지 충전할 수 있어요</h2>
-                        <div className="relative px-6 my-8 group">
-                            <label
-                                htmlFor="myInput"
-                                className="absolute text-xs text-black transition-all duration-200 opacity-0 -top-5 group-focus-within:opacity-100"
-                            >
-                                충전 금액
-                            </label>
-                            <div className="relative">
-                                <input
-                                    id="myInput"
-                                    type="text"
-                                    value={value}
-                                    onChange={handleChange}
-                                    className="w-full px-3 py-2 border-b border-[#E0E0E0] focus:outline-none focus:border-[#00A862] inline pl-0"
-                                    placeholder="충전 금액(1만원 단위)"
-                                />
-                                <span className="absolute right-3 top-1/2 -translate-y-1/2 text-sm text-[#686868]">
-                                    만원
-                                </span>
-                            </div>
-                        </div>
-                        <button
-                            name="cancel-btn"
-                            onClick={() => {
-                                setSelectedUnit(10000);
-                                setIsModalOpen(false);
-                            }}
-                            className="w-1/2 px-4 py-4 font-semibold text-black border rounded-bl-lg"
-                        >
-                            취소
-                        </button>
-                        <button
-                            name="confirm-btn"
-                            onClick={() => {
-                                setSelectedUnit(Number(value) * 10000);
-                                modifyCustomUnitValue(Number(value) * 10000);
-                                setValue('');
-                                setIsModalOpen(false);
-                            }}
-                            disabled={!isButtonEnabled}
-                            className={`w-1/2 px-4 py-4 font-semibold border rounded-br-lg text-[#00A862] disabled:text-[#C0C0C0]`}
-                        >
-                            충전하기
-                        </button>
+                        <InputForm value={value} handleChange={handleChange} />
+                        <ModalButtonArea
+                            setSelectedUnit={setSelectedUnit}
+                            setIsModalOpen={setIsModalOpen}
+                            modifyCustomUnitValue={modifyCustomUnitValue}
+                            value={value}
+                            setValue={setValue}
+                        />
                     </div>
                 </div>
             )}
+        </div>
+    );
+};
+
+const InputForm = ({ value, handleChange }) => {
+    return (
+        <div className="relative px-6 my-8 group">
+            <label
+                htmlFor="myInput"
+                className="absolute text-xs text-black transition-all duration-200 opacity-0 -top-5 group-focus-within:opacity-100"
+            >
+                충전 금액
+            </label>
+            <div className="relative">
+                <input
+                    id="myInput"
+                    type="text"
+                    value={value}
+                    onChange={handleChange}
+                    className="w-full px-3 py-2 border-b border-[#E0E0E0] focus:outline-none focus:border-[#00A862] inline pl-0"
+                    placeholder="충전 금액(1만원 단위)"
+                />
+                <span className="absolute right-3 top-1/2 -translate-y-1/2 text-sm text-[#686868]">만원</span>
+            </div>
+        </div>
+    );
+};
+
+const ModalButtonArea = ({ setSelectedUnit, setIsModalOpen, modifyCustomUnitValue, value, setValue }) => {
+    const isButtonEnabled = value.trim() !== '';
+
+    return (
+        <div>
+            <button
+                name="cancel-btn"
+                onClick={() => {
+                    setSelectedUnit(10000);
+                    setIsModalOpen(false);
+                }}
+                className="w-1/2 px-4 py-4 font-semibold text-black border rounded-bl-lg"
+            >
+                취소
+            </button>
+            <button
+                name="confirm-btn"
+                onClick={() => {
+                    setSelectedUnit(Number(value) * 10000);
+                    modifyCustomUnitValue(Number(value) * 10000);
+                    setValue('');
+                    setIsModalOpen(false);
+                }}
+                disabled={!isButtonEnabled}
+                className={`w-1/2 px-4 py-4 font-semibold border rounded-br-lg text-[#00A862] disabled:text-[#C0C0C0]`}
+            >
+                충전하기
+            </button>
         </div>
     );
 };
