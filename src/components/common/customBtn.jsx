83119ca929fc6c2301815cbdb6f07f47ed587b77
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-06-16 21:44:36 +0900

    feat: CommonBtn 옵션 추가
    CommonBtn 컴포넌트에 비활성화 상태와 폰트 굵기를 커스텀할 수 있는 옵션을 추가했다.

--- src/components/common/customBtn.jsx
+++ src/components/common/customBtn.jsx
@@ -11,6 +11,8 @@ export function CommonBtn({
     paddingY = 'py-3',
     onClick,
     className = '',
+    disabled = false,
+    weight = 'font-semibold',
 }) {
     return (
         <button
@@ -20,7 +22,7 @@ export function CommonBtn({
         ${paddingY} ${paddingX}
         rounded-full
         font-sans
-        font-semibold
+        ${weight}
         cursor-pointer
         border border-[#01A862]
         transition duration-300 ease-in-out
@@ -29,6 +31,7 @@ export function CommonBtn({
         ${className}
       `}
             onClick={onClick}
+            disabled={disabled}
         >
             {title}
         </button>
