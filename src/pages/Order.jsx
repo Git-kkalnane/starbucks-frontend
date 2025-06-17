0a6a9ede3ccde5bda745a1022fca190873a7a07a
Cori1304-Seong <andyjm7739@gmail.com>
2025-06-17 23:36:43 +0900

    feat: 임시 API 적용
    1. 기존에 사용던 mock up data 삭제

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -4,34 +4,47 @@ import { ColumnHeader } from '../components/common/customHeader';
 import MenuTab from '../components/order/MenuTab';
 import MenuList from '../components/order/MenuList';
 import OrderNavigationBar from '../components/order/OrderNavigationBar';
-import { getDrinkItems, getFoodItems } from '../services/OrderService';
+import { OrderQueryService } from '../services/OrderService';
 
 function Order() {
-    const [menuData, setMenuData] = useState({
-        drinkItems: [],
-        foodItems: [],
-    });
+    const [activeTab, setActiveTab] = useState('음료');
+    const [allDrinks, setAllDrinks] = useState([]);
+    const [allDesserts, setAllDesserts] = useState([]);
 
     useEffect(() => {
-        const loadMenuData = () => {
-            setMenuData({
-                drinkItems: getDrinkItems(),
-                foodItems: getFoodItems(),
-            });
-        };
+        async function fetchData() {
+            try {
+                const drinks = await OrderQueryService.fetchDrinkItems();
+                console.log(drinks[0]);
+                setAllDrinks(drinks);
+            } catch (err) {
+                console.error('데이터 불러오기 실패:', err);
+            }
+        }
 
-        loadMenuData();
+        fetchData();
     }, []);
 
-    const { drinkItems, foodItems } = menuData;
-    const [activeTab, setActiveTab] = useState('음료');
+    useEffect(() => {
+        async function fetchDessertData() {
+            try {
+                const desserts = await OrderQueryService.fetchDessertItems();
+                console.log(desserts[0]);
+                setAllDesserts(desserts);
+            } catch (err) {
+                console.error('데이터 불러오기 실패:', err);
+            }
+        }
+
+        fetchDessertData();
+    }, []);
 
     return (
         <CommonLayout>
             <ColumnHeader title="Order" className="pt-[68px]" />
             <div className="px-5 ">
                 <MenuTab activeTab={activeTab} onTabChange={setActiveTab} />
-                <MenuList activeTab={activeTab} drinkItems={drinkItems} foodItems={foodItems} />
+                <MenuList activeTab={activeTab} drinkItems={allDrinks} dessertItems={allDesserts} />
             </div>
             <OrderNavigationBar itemCount={5} />
         </CommonLayout>
